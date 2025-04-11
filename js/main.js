1aa47fbf0c48cadbd03a4f6bc771c8120e9850ec
GustavoScotton <gustavoscotton@hotmail.com>
2025-04-11 16:24:59 -0300

    Atualizando HTML

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    if (!localStorage.getItem('usuarios')) {
-      fetch('data/exemplos.json')
-        .then(r => r.json())
-        .then(d => localStorage.setItem('usuarios', JSON.stringify(d.usuarios)));
-    }
-  };
