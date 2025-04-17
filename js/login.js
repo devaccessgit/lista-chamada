fcefb31af9b166b8207b6f3603cf7f432c002d70
devaccessgit <managergithubaccess@outlook.com>
2025-04-17 16:21:25 -0300

    Renomeando lista-chamada.html para index.html

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const email = document.getElementById('emailLogin').value.trim();
+    const usuario = document.getElementById('usuarioLogin').value.trim();
     const senha = document.getElementById('senhaLogin').value.trim();
 
-    // Verificar se o campo de email ou senha está vazio
-    if (!email || !senha) {
+    // Verificar se o campo de nome de usuário ou senha está vazio
+    if (!usuario || !senha) {
         alert('Por favor, preencha todos os campos!');
         return;
     }
@@ -14,27 +14,27 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
     // Cadastrar usuário admin caso não exista
-    if (!usuarios.find(u => u.email === 'admin@admin.com')) {
+    if (!usuarios.find(u => u.usuario === 'admin')) {
         usuarios.push({
-            email: 'admin@admin.com',
+            usuario: 'admin',
             senha: btoa('admin123'), // Senha encriptada
             tipo: 'admin'
         });
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
     }
 
-    // Buscar usuário com base no email e senha fornecidos
-    const usuario = usuarios.find(u => u.email === email && u.senha === btoa(senha)); // Comparar a senha encriptada
+    // Buscar usuário com base no nome de usuário e senha fornecidos
+    const usuarioLogado = usuarios.find(u => u.usuario === usuario && u.senha === btoa(senha)); // Comparar a senha encriptada
 
     const errorMessageDiv = document.getElementById('login-error');
 
-    if (usuario) {
-        localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
+    if (usuarioLogado) {
+        localStorage.setItem('usuarioLogado', JSON.stringify(usuarioLogado));
 
         // Redirecionar conforme o tipo de usuário
-        if (usuario.tipo === 'admin') {
+        if (usuarioLogado.tipo === 'admin') {
             window.location.href = 'admin.html';
-        } else if (usuario.tipo === 'professor') {
+        } else if (usuarioLogado.tipo === 'professor') {
             window.location.href = 'professor.html';
         } else {
             alert('Tipo de usuário desconhecido!');
